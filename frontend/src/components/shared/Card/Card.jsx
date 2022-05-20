cd92021a169e3bd96083b4c4f53ee986aa3f6370
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2022-05-21 01:28:43 +0530

    Auto Login, Loader and Logout Functionality
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- frontend/src/components/shared/Card/Card.jsx
+++ frontend/src/components/shared/Card/Card.jsx
@@ -5,8 +5,8 @@ const Card = ({ title, icon, children }) => {
     return (
         <div className={styles.card}>
             <div className={styles.headingWrapper}>
-                <img className='logo' src={`/images/${icon}.png`} alt="logo" />
-                <h1 className={styles.heading}>{title}</h1>
+            {icon && <img src={`/images/${icon}.png`} alt="logo" />}
+            {title && <h1 className={styles.heading}>{title}</h1>}
             </div>
             {children}
         </div>
--- frontend/src/components/shared/Navigation/Navigation.jsx
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { logout } from '../../../http';
 import styles from './Navigation.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { setAuth } from '../../../store/authSlice';
 
 const Navigation = () => {
     const brandStyle = {
@@ -15,13 +18,24 @@ const Navigation = () => {
     const logoText = {
         marginLeft: '10px',
     };
+    const dispatch = useDispatch();
+    const { isAuth } = useSelector((state) => state.auth);
+    async function logoutUser() {
+        try {
+            const { data } = await logout();
+            dispatch(setAuth(data));
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
     return (
         <nav className={`${styles.navbar} container`}>
             <Link style={brandStyle} to="/">
                 <img src="/images/logo.png" alt="logo" />
-                <span style={logoText}>StreamHouse</span>
+                <span style={logoText}>Codershouse</span>
             </Link>
+            {isAuth && <button onClick={logoutUser}>Logout</button>}
         </nav>
     );
 };
